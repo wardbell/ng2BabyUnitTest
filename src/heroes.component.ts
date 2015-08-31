adf85131f9a7e6d3094e63485151852b00a4eb60
John Papa <john@johnpapa.net>
2015-08-31 18:10:43 -0400

    separated css and html from heroes component

--- src/heroes.component.ts
+++ src/heroes.component.ts
@@ -8,46 +8,32 @@ import {User} from 'user';
   selector: 'heroes'
 })
 @View({
-    template: `
-      <div id="output">
-        <h1>{{userName}}'s Heroes</h1>
-        <button (click)="onRefresh(h)">Refresh</button>
-        <ul class="heroes">
-          <li *ng-for="#hero of heroes" (click)="onSelect(hero)">
-            ({{hero.id}}) {{hero.name}}
-          </li>
-        </ul>
-        <div *ng-if="currentHero">
-          <hr/>
-          <hero [hero]="currentHero" [user-name]="userName" (delete)="onDelete()"></hero>
-        </div>
-      </div>
-    `,
-    directives: [HeroComponent, NgFor, NgIf],
-    styles: ['.heroes {list-style-type: none; margin-left: 1em; padding: 0}']
+  templateUrl: 'heroes.component.html',
+  directives: [HeroComponent, NgFor, NgIf],
+  styleUrls: ['heroes.component.css']
 })
 export class HeroesComponent {
   private _heroes: Hero[];
 
-  constructor(private _heroDataService: HeroDataservice, private _user:User) {  }
+  constructor(private _heroDataService: HeroDataservice, private _user: User) { }
 
-  currentHero:Hero;
+  currentHero: Hero;
 
   get heroes() {
     if (!this._heroes) { this._getAllHeroes(); }
     return this._heroes;
   }
 
-  onSelect(hero:Hero) {
+  onSelect(hero: Hero) {
     this.currentHero = hero;
     console.log(`Hero selected: ${hero.name}`);
   }
 
-  onDelete(hero:Hero){
+  onDelete(hero: Hero) {
     hero = hero || this.currentHero;
     let ix = this._heroes.indexOf(hero);
     this._heroDataService.removeHero(hero);
-    this.currentHero = this._heroes[ix] || this._heroes[ix-1];
+    this.currentHero = this._heroes[ix] || this._heroes[ix - 1];
   }
 
   onRefresh() {
@@ -60,7 +46,7 @@ export class HeroesComponent {
   get userName() { return this._user.name || 'someone'; }
 
   /////////////
-  private _getAllHeroes(force:boolean = false) {
+  private _getAllHeroes(force: boolean = false) {
     this._heroDataService.getAllHeroes(force).then(heroes => {
       this._heroes = heroes;
       if (!this.currentHero) { this.currentHero = heroes[0]; }
