9ada4506e9cf47af9692ed059ffcb6b5cde1233f
John Papa <john@johnpapa.net>
2015-08-31 19:11:11 -0400

    WIP: Commented out code for getting HTTP started. Not ready yet.

--- src/core.ts
+++ src/core.ts
@@ -1,3 +1,7 @@
+//TODO: rename core to core.injectables.ts
+
+// import {ConnectionBackend, Http, RequestOptions} from 'http/http';
+// import {Http} from 'angular2/http';
 import {Backend} from 'backend';
 import {HeroDataservice} from 'hero.dataservice';
 import {User} from 'user';
